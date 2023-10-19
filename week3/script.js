757c3a2b0bb49a34c2b864d07e767f6d3d4bdcf5
MHamzaK7 <145095465+MHamzaK7@users.noreply.github.com>
2023-10-19 16:56:06 +0400

    Add files via upload

--- week3/script.js
+++ week3/script.js
@@ -1,24 +1,40 @@
-let currentSlide = 1;
-
-function showSlide(index) {
-    const slides = document.getElementsByClassName("mySlides");
-    if (index < 1) {
-        currentSlide = slides.length;
-    }
-    else if (index > slides.length) {
-        currentSlide = 1;
-    }
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[currentSlide - 1].style.display = "block";
-}
-
-function changeSlide(i) {
-    currentSlide += i;
+document.addEventListener("DOMContentLoaded", function () {
+    // Initializing the current slide
+    let currentSlide = 1;
+    // Initializing the first shown slide
     showSlide(currentSlide);
-}
 
-showSlide(currentSlide);
+    // Function to handle the showing of slides
+    function showSlide(index) {
+        const slides = document.getElementsByClassName("mySlides");
+        if (index < 1) {
+            currentSlide = slides.length;
+        } else if (index > slides.length) {
+            currentSlide = 1;
+        }
+
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        // Displaying the current slide
+        slides[currentSlide - 1].style.display = "block";
+    }
+
+    // Using indexes to change slides
+    function changeSlide(i) {
+        currentSlide += i;
+        showSlide(currentSlide);
+    }
+
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
+
+    prevButton.addEventListener("click", function () {
+        changeSlide(-1);
+    });
+
+    nextButton.addEventListener("click", function () {
+        changeSlide(1);
+    });
+});
--- week3/signup.js
+++ week3/signup.js
@@ -1,102 +1,93 @@
-const usernameInput = document.getElementById("username");
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const confirmPasswordInput = document.getElementById("confirm-password");
-const usernameError = document.getElementById("username-error");
-const emailError = document.getElementById("email-error");
-const passwordError = document.getElementById("password-error");
-const confirmPasswordError = document.getElementById("confirm-password-error");
-const submitButton = document.getElementById("submit-button");
+document.addEventListener("DOMContentLoaded", function () {
+    const usernameInput = document.getElementById("username");
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const confirmPasswordInput = document.getElementById("confirm-password");
+    const usernameError = document.getElementById("username-error");
+    const emailError = document.getElementById("email-error");
+    const passwordError = document.getElementById("password-error");
+    const confirmPasswordError = document.getElementById("confirm-password-error");
+    const submitButton = document.getElementById("submit-button");
 
-usernameInput.addEventListener("blur", validateUsername);
-emailInput.addEventListener("blur", validateEmail);
-passwordInput.addEventListener("blur", validatePassword);
-confirmPasswordInput.addEventListener("blur", validateConfirmPassword);
-submitButton.addEventListener("blur", validateSubmitForm);
+    // Using Event Listeners for user input fields
+    usernameInput.addEventListener("blur", validateUsername);
+    emailInput.addEventListener("blur", validateEmail);
+    passwordInput.addEventListener("blur", validatePassword);
+    confirmPasswordInput.addEventListener("blur", validateConfirmPassword);
+    submitButton.addEventListener("click", validateSubmitForm);
 
-function validateUsername() {
-    const usernameText = usernameInput.value.trim();
+    function validateUsername() {
+        const usernameText = usernameInput.value.trim();
 
-    if (usernameText === "") {
-        usernameInput.classList.add("error")
-        usernameError.textContent = "Username cannot be empty!"
-    }
-    else {
-        usernameInput.classList.remove("error");
-        usernameError.textContent = "";
-    }
-}
-
-function validateEmail() {
-    const emailText = emailInput.value.trim();
-    const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (emailText === "") {
-        emailInput.classList.add("error");
-        emailError.textContent = "Email cannot be empty!"
-    }
-    else if (!emailFormat.test(emailText)) {
-        emailInput.classList.add("error");
-        emailError.textContent = "Invalid email format!"
-    }
-    else {
-        emailInput.classList.remove("error");
-        emailError.textContent = "";
-    }
-}
-
-function validatePassword() {
-    const passwordText = passwordInput.value;
-
-    if (passwordText === "") {
-        passwordInput.classList.add("error");
-        passwordError.classList = "Password cannot be empty!"
-    }
-    else if (passwordText.length < 8) {
-        passwordInput.classList.add("error");
-        passwordError.textContent = "Password is too short!"
-    }
-    else {
-        passwordInput.classList.remove("error");
-        passwordError.textContent = "";
-    }
-}
-
-function validateConfirmPassword() {
-    const passwordText = passwordInput.value;
-    const confirmPasswordText = confirmPasswordInput.value;
-
-    if (confirmPasswordText ===""){
-        confirmPasswordInput.classList.add("error");
-        confirmPasswordError.textContent = "Confirm Password cannot be empty!";
-    }
-    else if (confirmPasswordText !== passwordText) {
-        confirmPasswordInput.classList.add("error");
-        confirmPasswordError.textContent = "Passwords do not match";
-    }
-    else {
-        confirmPasswordInput.classList.remove("error");
-        confirmPasswordError.textContent = "";
-    }
-}
-
-function validateSubmitForm() {
-    validateUsername();
-    validateEmail()
-    validatePassword();
-    validateConfirmPassword();
-
-    if (!usernameInput.classList.contains("error") &&
-        !emailInput.classList.contains("error") &&
-        !passwordInput.classList.contains("error") &&
-        !confirmPasswordInput.classList.contains("error")
-    ){
-        alert("Sign Up Successful!");
-
+        if (usernameText === "") {
+            usernameInput.classList.add("error")
+            usernameError.textContent = "Username cannot be empty!"
+        } else {
+            usernameInput.classList.remove("error");
+            usernameError.textContent = "";
+        }
     }
 
-}
+    function validateEmail() {
+        const emailText = emailInput.value.trim();
+        const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+        if (emailText === "") {
+            emailInput.classList.add("error");
+            emailError.textContent = "Email cannot be empty!"
+        } else if (!emailFormat.test(emailText)) {
+            emailInput.classList.add("error");
+            emailError.textContent = "Invalid email format!"
+        } else {
+            emailInput.classList.remove("error");
+            emailError.textContent = "";
+        }
+    }
 
+    function validatePassword() {
+        const passwordText = passwordInput.value;
 
+        if (passwordText === "") {
+            passwordInput.classList.add("error");
+            passwordError.textContent = "Password cannot be empty!"
+        } else if (passwordText.length < 8) {
+            passwordInput.classList.add("error");
+            passwordError.textContent = "Password is too short!"
+        } else {
+            passwordInput.classList.remove("error");
+            passwordError.textContent = "";
+        }
+    }
 
+    function validateConfirmPassword() {
+        const passwordText = passwordInput.value;
+        const confirmPasswordText = confirmPasswordInput.value;
+
+        if (confirmPasswordText === "") {
+            confirmPasswordInput.classList.add("error");
+            confirmPasswordError.textContent = "Confirm Password cannot be empty!";
+        } else if (confirmPasswordText !== passwordText) {
+            confirmPasswordInput.classList.add("error");
+            confirmPasswordError.textContent = "Passwords do not match";
+        } else {
+            confirmPasswordInput.classList.remove("error");
+            confirmPasswordError.textContent = "";
+        }
+    }
+
+    function validateSubmitForm(event) {
+        event.preventDefault();
+        validateUsername();
+        validateEmail();
+        validatePassword();
+        validateConfirmPassword();
+
+        if (!usernameInput.classList.contains("error") &&
+            !emailInput.classList.contains("error") &&
+            !passwordInput.classList.contains("error") &&
+            !confirmPasswordInput.classList.contains("error")
+        ) {
+            alert("Sign Up Successful!");
+        }
+    }
+});
